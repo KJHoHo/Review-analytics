1f72b69ea8adf61f375671bcfb6dd5a76999a7a5
KJHo <africancc@163.com>
2021-10-22 08:57:42 +0800

    calc avg length reviews

--- read.py
+++ read.py
@@ -7,6 +7,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print(len(data))
-print(data[0])
-print('------------------')
-print(data[1])
+print('档案读取完了，总共有', len(data), '笔资料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len +len(d)
+
+print('留言的平均长度为', sum_len / len(data))
